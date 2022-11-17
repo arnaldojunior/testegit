d7c7378aafc10b0d1d951725433011fd13233e67
Arnaldo Junior <arnaldojunior1991@gmail.com>
2022-11-17 11:10:16 -0300

    Implementação das funções de somar e subtrair.

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 function exibirMsg() {
     alert('Olá!');
 }
+
+function somar(v1, v2) {
+    return parseFloat(v1) + parseFloat(v2);
+}
+
+function subtrair(v1, v2) {
+    return parseFloat(v1) - parseFloat(v2);
+}
